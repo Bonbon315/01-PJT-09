3513da3f46180158aaf5bc30f8ea9d0c065c65d9
Bonbon315 <n202u123@gmail.com>
2022-10-07 11:26:34 +0900

    20221007 pair pjt

--- articles/models.py
+++ articles/models.py
@@ -3,15 +3,16 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 class Article(models.Model):
-    title = models.CharField(max_length=50)
-    content = models.TextField()
-    movie_name = models.CharField(max_length=50)
+    title = models.CharField(max_length=50, verbose_name="리뷰 제목")
+    content = models.TextField(verbose_name="리뷰 내용")
+    movie_name = models.CharField(max_length=50, verbose_name="영화 이름")
     grade = models.IntegerField(
         default=1,
         validators=[
             MaxValueValidator(5),
             MinValueValidator(1),
         ],
+        verbose_name="평점",
     )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- articles/urls.py
+++ articles/urls.py
@@ -4,5 +4,10 @@ from . import views
 app_name = "articles"
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.home, name="home"),
+    path("index/", views.index, name="index"),
+    path("<int:pk>/", views.detail, name="detail"),
+    path("<int:pk>/update/", views.update, name="update"),
+    path("<int:pk>/delete/", views.delete, name="delete"),
+    path("create/", views.create, name="create"),
 ]
